6476d7402856fbe5eb7ed6837ae831cf0271e27f
Andriy <andri0667365973@gmail.com>
2025-10-24 20:58:08 +0300

    fix:build fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = { productionBrowserSourceMaps: true };
+const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+};
 
 const nextConfigWithIntl = createNextIntlPlugin({
   requestConfig: "./src/pkg/libraries/locale/request.ts",
