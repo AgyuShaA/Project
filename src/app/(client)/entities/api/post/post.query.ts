96d51335050a37509db68fe0bdef1f9e16db8255
Andriy <andri0667365973@gmail.com>
2025-10-24 19:01:02 +0300

    feat:query init

---
+++ src/app/(client)/entities/api/post/post.query.ts
@@ -0,0 +1,29 @@
+import { queryOptions } from "@tanstack/react-query";
+import postQueryApi from "./post.api";
+import { Post } from "../../models";
+
+export const postQueryOptions = () => {
+  return queryOptions({
+    queryKey: ["posts"],
+    queryFn: (params) => {
+      const data = postQueryApi(params) as Promise<Post[]>;
+
+      return data;
+    },
+    staleTime: 30_000,
+    refetchInterval: 30_000,
+  });
+};
+
+export const postQueryOptionsById = (id: string) => {
+  return queryOptions({
+    queryKey: ["post", id],
+    queryFn: (params) => {
+      const data = postQueryApi(params) as Promise<Post>;
+
+      return data;
+    },
+    staleTime: 30_000,
+    refetchInterval: 30_000,
+  });
+};
