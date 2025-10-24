4f8bfdc787158ee1ff113ad902092f7112bdea12
Andriy <andri0667365973@gmail.com>
2025-10-24 19:22:52 +0300

    refactor:refactor code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ if (!process.env.DATABASE_URL) throw new Error("Missing DATABASE_URL");
 export default defineConfig({
   schema: "./src/pkg/integrations/better-auth/lib/auth-schema.ts",
   out: "./drizzle/migrations",
-  dialect: "postgresql", // <- required
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
