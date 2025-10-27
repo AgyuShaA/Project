684d71ade0d32c798c41bb3ac987ce31927fee3e
Andriy <andri0667365973@gmail.com>
2025-10-27 09:52:43 +0200

    refactor:added reimports

---
+++ src/app/(client)/(app)/features/index.ts
@@ -0,0 +1,6 @@
+export { LoginFormComponent } from "./login-form";
+export { RegisterFormComponent } from "./register-form";
+
+export { default as PostCardDynamic } from "./post/post-card-dynamic.component";
+export { default as PostList } from "./post/post-list.component";
+export { default as PostListFilters } from "./post/post-filters.component";
