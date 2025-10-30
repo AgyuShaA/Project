6c4c1ddf6dd4f592bf8b07b2ce2b928149622d9d
Andriy <andri0667365973@gmail.com>
2025-10-30 11:38:02 +0200

    ref:refactor card-details compoennt

--- src/app/(client)/(app)/features/post/post-card-details/post-card-details.component.tsx
+++ src/app/(client)/(app)/features/post/post-card-details/post-card-details.component.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { Card } from "../../../shared/ui/card";
+import {
+  Card,
+  CardContentComponent,
+  CardHeaderComponent,
+  CardTitleComponent,
+} from "../../../shared/ui/card";
 import { postQueryOptionsById } from "../../../entities/api/post";
 import { Post } from "../../../entities/models";
 
@@ -15,9 +20,13 @@ export default function PostCardDetails({ id }: PostCardProps) {
 
   return (
     <Card className="flex flex-col items-center gap-4 rounded-lg border border-gray-200 p-6 shadow-sm">
-      <h2 className="text-center text-xl font-bold">{post?.title}</h2>
+      <CardHeaderComponent className="text-center text-xl font-bold">
+        <CardTitleComponent>{post?.title}</CardTitleComponent>
+      </CardHeaderComponent>
 
-      <p className="text-center text-gray-700">{post?.body}</p>
+      <CardContentComponent>
+        <p className="text-center text-gray-700">{post?.body}</p>
+      </CardContentComponent>
     </Card>
   );
 }
