5f8c24ae10bb36f228b6b8fefb7ff60f75422e19
Andriy <andri0667365973@gmail.com>
2025-11-06 12:34:59 +0200

    feat:redis to store sessions

--- src/pkg/integrations/better-auth/lib/auth.ts
+++ src/pkg/integrations/better-auth/lib/auth.ts
@@ -3,6 +3,11 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as shema from "./auth-schema";
 import { db } from "../../drizzle/server";
+import { createClient } from "redis";
+
+const redis = createClient({ url: process.env.UPSTASH_REDIS_URL });
+
+await redis.connect();
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -14,6 +19,21 @@ export const auth = betterAuth({
       verification: shema.verification,
     },
   }),
+  	secondaryStorage: {
+    useFor: ["session"],
+
+		get: async (key) => {
+			return await redis.get(key);
+		},
+		set: async (key, value, ttl) => {
+      console.log(key,value,ttl)
+			if (ttl) await redis.set(key, value, { EX: ttl });
+			else await redis.set(key, value);
+		},
+		delete: async (key) => {
+			await redis.del(key);
+		}
+	},
   emailAndPassword: {
     enabled: true,
   },
